b8745cf3559a5dd037d1a273fbd3038f4518274f
Redhatt <45532454+Redhatt@users.noreply.github.com>
2021-04-23 23:54:07 +0530

    Update execute.sh

--- Topics/execute.sh
+++ Topics/execute.sh
@@ -7,15 +7,15 @@ touch worker_b.txt
 count=10
 for i in $(seq $count); do
     mpirun -n 3 python MPI-matrix-multiply/Topics/code_a.py
-    mpirun -n 3 MPI-matrix-multiply/Topics/python code_b.py
+    mpirun -n 3 python MPI-matrix-multiply/Topics/code_b.py
 done
 
 for i in $(seq $count); do
-    mpirun -n 5 MPI-matrix-multiply/Topics/python code_a.py
-    mpirun -n 5 MPI-matrix-multiply/Topics/python code_b.py
+    mpirun -n 5 python MPI-matrix-multiply/Topics/code_a.py
+    mpirun -n 5 python MPI-matrix-multiply/Topics/code_b.py
 done
 
 for i in $(seq $count); do
-    mpirun -n 7 MPI-matrix-multiply/Topics/python code_a.py
-    mpirun -n 7 MPI-matrix-multiply/Topics/python code_b.py
+    mpirun -n 7 python MPI-matrix-multiply/Topics/code_a.py
+    mpirun -n 7 python MPI-matrix-multiply/Topics/code_b.py
 done
