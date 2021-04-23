16f0fb69d51a448e32484d32708c22adf9eec38c
Redhatt <45532454+Redhatt@users.noreply.github.com>
2021-04-23 23:37:15 +0530

    bug fix

--- Topics/execute.sh
+++ Topics/execute.sh
@@ -6,18 +6,18 @@ touch worker_b.txt
 
 count=10
 for i in $(seq $count); do
-    mpirun -n 3 python code_a.py
-    mpirun -n 3 python code_b.py
+    mpirun -n 3 python MPI-matrix-multiply/Topics/code_a.py
+    mpirun -n 3 MPI-matrix-multiply/Topics/python code_b.py
 done
 
 for i in $(seq $count); do
-    mpirun -n 5 python code_a.py
-    mpirun -n 5 python code_b.py
+    mpirun -n 5 MPI-matrix-multiply/Topics/python code_a.py
+    mpirun -n 5 MPI-matrix-multiply/Topics/python code_b.py
 done
 
 for i in $(seq $count); do
-    mpirun -n 7 python code_a.py
-    mpirun -n 7 python code_b.py
+    mpirun -n 7 MPI-matrix-multiply/Topics/python code_a.py
+    mpirun -n 7 MPI-matrix-multiply/Topics/python code_b.py
 done
 
-python result.py
+python MPI-matrix-multiply/Topics/result.py
