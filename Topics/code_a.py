495be4a5f874284ef7043edd89164e9cac9a9c22
Redhatt <45532454+Redhatt@users.noreply.github.com>
2021-04-25 12:33:39 +0530

    Update code_a.py

--- Topics/code_a.py
+++ Topics/code_a.py
@@ -43,7 +43,7 @@ if rank == 0:
 		fp.write("{}-{}".format(N, total_time))
 
 for i in range(N):
-	if rank != 0:
+	if rank == i+1:
 		matrix = np.empty((m//N, n))
 		com.Recv(matrix, source=0, tag=i)
 
@@ -52,4 +52,5 @@ for i in range(N):
 
 		# sending answer
 		com.Send(ans, dest=0)
+
 
